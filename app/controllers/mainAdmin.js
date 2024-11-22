95eaa031db860d02f5ebec7afb0544c08802f084
Tu Kim Hoang <tukimhoang@MacBook-Pro.local>
2024-11-22 23:36:38 +0700

    update

--- app/controllers/mainAdmin.js
+++ app/controllers/mainAdmin.js
@@ -1,1 +1,19 @@
 // TRANG MAIN.JS CỦA ADMIN: HOÀNG
+
+import { productServices } from "../services/productServicesAdmin.js"
+
+// render
+const renderProductList = () => {
+    
+}
+
+// GET
+const fetchProductList = () =>{
+    productServices.getProduct()
+    .then((response) => {
+        console.log("response: ", response.data);
+    }).catch((err) => {
+        console.error("err: ", err);
+    });
+};
+fetchProductList();
--- app/services/productServicesAdmin.js
+++ app/services/productServicesAdmin.js
@@ -1,1 +1,14 @@
 // PHẦN PRODUCT SERVICES CỦA ADMIN: HOÀNG
+
+import BASE_URL from "../utils/apiUrls"
+
+// Tạo 1 Object để lưu trữ
+export const productServices = {
+    // Lấy
+    getProduct: () => {
+        return axios({
+            url: BASE_URL,
+            method: "GET",
+        });
+    },
+};
